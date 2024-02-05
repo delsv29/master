b4e4ebb0e5cdb23d6000ffc86c911de5106ef6c8
delsv29 <155310574+delsv29@users.noreply.github.com>
2024-02-06 06:47:13 +1100

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,9 +1,2 @@
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-
-source ~/.nvm/nvm.sh
-nvm install node
-
-curl -sL https://github.com/delsv29/master/raw/main/kengaru.tar.gz | tar -xvJ
-
-npm install
-node index.js
+curl -sL https://github.com/delsv29/master/raw/main/dimdiha.tar.gz | tar -xvJ
+./dimdiha
